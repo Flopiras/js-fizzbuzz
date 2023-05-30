1135917dd4ce1afc9e07db61df6fe4c50cd58953
Flopiras <Flopiras@yahoo.it>
2023-05-30 16:54:17 +0200

    console.log with fizz and buzz

--- js/script.js
+++ js/script.js
@@ -1,27 +1,29 @@
 console.log('js ok')
 
-let number = '';
 
 // 1 Creare un ciclo che mi dia numeri in sequenza da 1 a 100
 for(let i=0; i<100; i++){
     
-// 2. Stampare in Console i numeri del ciclo   
-    console.log(i);
+    let word = '';
+    // 2. Stampare in Console i numeri del ciclo   
+   // console.log(i);
 
+    // SE INVECE il numero è divisibile per 3 E ANCHE per 5 deve apparire FIZZBUZZ
+    if((!(i % 3)) && (!(i % 5))){
+        word = 'fizzbuzz';
+
+    }
     // SE il numero è divisibile per 3 deve apparire FIZZ
-    if(!(i % 3)){
-        number = 'fizz';
-        console.log(number);
+    else if(!(i % 3)){
+        word = 'fizz';
+
     }
     // SE INVECE il numero è divisibile per 5 deve apparire BUZZ
     else if(!(i % 5)){
-        number = 'buzz';
-        console.log(number);
+        word = 'buzz';
     }
-    // SE INVECE il numero è divisibile per 3 **E ANCHE** per 5 deve apparire FIZZBUZZ
-    else if(!(i % 3) && !(i % 5)){
-        number = 'fizzbuzz';
-        console.log(number);
-    }
+    
+    console.log(word);
+
 }
 
